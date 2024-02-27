bc92a1ced082401f26ef700838c740818495c7f4
holy fucking shit <154252167+randomuser691337@users.noreply.github.com>
2024-02-27 16:30:09 -0600

    2nd commit :3

--- Gooper.bat
+++ Gooper.bat
@@ -1,15 +1,16 @@
 @echo off
 :START
-echo Gooper 1.0 - made by randomuser691337 :3
-set /p extension="- Username and repository (e.g. user/repo)? "
-set /p location="- Location? (e.g. Desktop)?"
+echo Gooper 2.0 - made by randomuser691337 :3
+set /p user="- Username of repo maker (e.g. torvalds)? "
+set /p extension="- Repository of user (e.g. linux)? "
+set /p location="- Location? (e.g. Desktop)? "
 set "location=%location:\=/%"
 @cd "%USERPROFILE%\%location%"
 if exist "%USERPROFILE%\%location%\%extension%" (
     echo Removing existing directory...
     rd /s /q "%USERPROFILE%\%location%\%extension%"
 )
-git clone --no-checkout https://github.com/%extension%
+git clone --no-checkout https://github.com/%user%/%extension%
 @cd "%USERPROFILE%\%location%\%extension%"
 if %errorlevel% equ 0 (
     icacls "%USERPROFILE%\%location%\%extension%" /grant "%USERNAME%:(OI)(CI)F" /T
